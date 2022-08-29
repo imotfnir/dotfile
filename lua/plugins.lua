6d2178d407fff554766d563c616cde2119d5c12e
imotfnir <imotfnir@gmail.com>
2022-08-30 01:49:56 +0800

    Change color schemes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,7 +16,6 @@ end
 
 -- Customized setting
 vim.cmd([[
-colorscheme tokyonight
 set number
 set relativenumber
 set autoindent
@@ -54,14 +53,46 @@ vnoremap <A-k> :m '<-2<CR>gv=gv
 ]])
 
 
--- Show whitespace
--- vim.cmd([[
--- set listchars=eol:↓,tab:→ ,trail:~,extends:>,precades:<,space:·
--- set list
--- ]])
+-- Color schemes setting
+require('nightfox').setup({
+  options = {
+    -- Compiled file's destination location
+    compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    compile_file_suffix = "_compiled", -- Compiled file suffix
+    transparent = false,    -- Disable setting background
+    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    dim_inactive = false,   -- Non focused panes set to alternative background
+    styles = {              -- Style to be applied to different syntax groups
+      comments = "italic",    -- Value is any valid attr-list value `:help attr-list`
+      conditionals = "NONE",
+      constants = "NONE",
+      functions = "NONE",
+      keywords = "bold",
+      numbers = "NONE",
+      operators = "NONE",
+      strings = "NONE",
+      types = "italic,bold",
+      variables = "NONE",
+    },
+    inverse = {             -- Inverse highlight for different types
+      match_paren = false,
+      visual = false,
+      search = false,
+    },
+    modules = {             -- List of various plugins and additional options
+      -- ...
+    },
+  },
+  palettes = {},
+  specs = {},
+  groups = {},
+})
+-- setup must be called before loading
+vim.cmd([[colorscheme terafox]])
 
--- All Plugin Config
+
 vim.cmd [[packadd packer.nvim]]
+-- All Plugin Config
 return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim' -- Have packer manage itself
@@ -69,6 +100,7 @@ return require('packer').startup(function()
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
   -- You add plugins here  
-  use 'folke/tokyonight.nvim'
+  use "EdenEast/nightfox.nvim" -- Packer
 end)
+
 
