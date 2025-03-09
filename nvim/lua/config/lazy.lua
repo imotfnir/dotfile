2ed1932a6bc341b29802431ace3f527683f7d067
rigel_pang <imotfnir@gmail.com>
2025-03-09 22:28:33 +0000

    refactor(neovim): split plugins with extensive configurations

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,34 +32,9 @@ require("lazy").setup({
       },
     },
     { 'echasnovski/mini.nvim', version = '*' },
+    { "meznaric/key-analyzer.nvim", opts = {} },
     -- Completion
-    {
-      "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-      lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-      dependencies = {
-        -- main one
-        { "ms-jpq/coq_nvim", branch = "coq" },
-        -- 9000+ Snippets
-        { "ms-jpq/coq.artifacts", branch = "artifacts" },
-        -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-        -- Need to **configure separately**
-        { 'ms-jpq/coq.thirdparty', branch = "3p" }
-        -- - shell repl
-        -- - nvim lua api
-        -- - scientific calculator
-        -- - comment banner
-        -- - etc
-      },
-      init = function()
-        vim.g.coq_settings = {
-            auto_start = true, -- if you want to start COQ at startup
-            -- Your COQ settings here
-        }
-      end,
-      config = function()
-        -- Your LSP settings here
-      end,
-    }
+    { import = "plugins.completion" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
