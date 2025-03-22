a8dd72fa87ad2364f92d1c8b86d65c06a2ed917b
rigel_pang <imotfnir@gmail.com>
2025-03-22 06:49:00 +0000

    feat(zsh): add plugins zoxide

--- plugins.zsh
+++ plugins.zsh
@@ -20,7 +20,13 @@ plugins=(
     systemd
     fzf
     fzf-tab
+    zoxide
 )
 
 precmd () { echo -n "\x1b]1337;CurrentDir=$(pwd)\x07" }
 
+if [[ -z "${__zoxide_z+x}" ]]; then
+    cd () {
+        __zoxide_z "$@"
+    }
+fi
