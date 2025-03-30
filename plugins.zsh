8022ac50569b76eb9fe07dfedec7435e92c29b95
rigel_pang <imotfnir@gmail.com>
2025-03-30 17:15:08 +0000

    feat(zsh): update zsh fzf plugins feature, support fzf preview

--- plugins.zsh
+++ plugins.zsh
@@ -20,7 +20,10 @@ plugins=(
     systemd
     fzf
     fzf-tab
+    eza
     zoxide
+    thefuck
+    entr
 )
 
 precmd () { echo -n "\x1b]1337;CurrentDir=$(pwd)\x07" }
@@ -30,3 +33,17 @@ if [[ -z "${__zoxide_z+x}" ]]; then
         __zoxide_z "$@"
     }
 fi
+
+# Advanced customization of fzf options via _fzf_comprun function
+# - The first argument to the function is the name of the command.
+# - You should make sure to pass the rest of the arguments to fzf.
+_fzf_comprun() {
+    local command=$1
+    shift
+    case "$command" in
+        cd)           fzf --preview 'eza -T --color=always {} | head -200' "$@" ;;
+        ssh)          fzf --preview 'dig {}' "$@" ;;
+        export|unset) fzf --preview "eval 'echo \$'{}" "$@" ;;
+        *)            fzf --preview 'batcat --color=always {}' "$@" ;;
+    esac
+}
